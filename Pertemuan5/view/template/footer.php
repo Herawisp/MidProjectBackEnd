b4fc27e757f61d3ee307bfa126f54a7107b4773a
Herawisp <138887819+Herawisp@users.noreply.github.com>
2025-02-08 21:29:44 +0700

    Delete Pertemuan5 directory

--- Pertemuan5/view/template/footer.php
+++
@@ -1,7 +0,0 @@
-<!DOCTYPE html>
-<html lang="en">
-<body>
-    <footer class="bg-gray-800 text-white text-center p-4">
-        <p>&copy; 2025 Your Name. Follow me on <a href="#" class="text-blue-400">Social Media</a>.</p>
-    </footer>
-</body>
